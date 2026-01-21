c952aa77eec9cd77e815418d5aee1717526f95a5
PavloPro7 <03ppcv@gmail.com>
2026-01-21 10:59:09 +0100

    did task.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,35 +4,31 @@ import 'package:google_fonts/google_fonts.dart';
 import 'providers/app_state.dart';
 import 'screens/home_screen.dart';
 
-void main() => 
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => AppState(),
-      child: const MyApp(),
-    ),
-  );
+void main() => runApp(
+  ChangeNotifierProvider(create: (context) => AppState(), child: const MyApp()),
+);
 
-  class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        title: 'RPG Life',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          useMaterial3: true,
-          colorSchemeSeed: Colors.pink,
-          textTheme: GoogleFonts.robotoTextTheme(),
-          brightness: Brightness.dark,
-        ),
-        darkTheme: ThemeData(
-          useMaterial3: true,
-          colorSchemeSeed: Colors.deepPurple,
-          textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
-          brightness: Brightness.dark,
-        ),
-        home: const HomeScreen(),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RPG Life',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.pink,
+        textTheme: GoogleFonts.robotoTextTheme(),
+        brightness: Brightness.dark,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.deepPurple,
+        textTheme: GoogleFonts.interTextTheme(ThemeData.dark().textTheme),
+        brightness: Brightness.dark,
+      ),
+      home: const HomeScreen(),
+    );
   }
+}
