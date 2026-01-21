3015c5111e1da1f9f6e712521a2725573073fb42
PavloPro7 <03ppcv@gmail.com>
2026-01-21 12:28:47 +0100

    updated skill.dart

--- lib/models/skill.dart
+++ lib/models/skill.dart
@@ -1,15 +1,32 @@
-class Task {
-  final String id;
-  final String title;
-  final String skillId;
-  final DateTime date;
-  bool isCompleted;
+import 'package:flutter/material.dart';
 
-  Task({
+class Skill {
+  final String id;
+  final String name;
+  final String category;
+  double xp;
+  final Color color;
+
+  Skill({
     required this.id,
-    required this.title,
-    required this.skillId,
-    required this.date,
-    this.isCompleted = false,
+    required this.name,
+    required this.category,
+    this.xp = 0.0,
+    required this.color,
   });
+
+  int get level => (xp / 100).floor() + 1;
+  double get progressInLevel => (xp % 10) / 10;
+
+  void addXp(double amount) {
+    xp += amount;
+    if (xp < 0) xp = 0;
+  }
+
+  void applyDailyPenalty(double amount) {
+    if (xp > 0) {
+      xp -= amount;
+      if (xp < 0) xp = 0;
+    }
+  }
 }
